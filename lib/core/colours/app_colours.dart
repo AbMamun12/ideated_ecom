890c8df0117537c4c552b4c406eef3412931b297
abdullah <abdullah.mamun8191@gmail.com>
2025-12-14 16:53:26 +0600

    splash screen, onboarding1,2,3 screen complete.

---
+++ lib/core/colours/app_colours.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColours {
+  static const backGroundColour = Color(0xFFFFFFFF);
+  static const primaryColour = Color(0xFFFF3758);
+  static const secondaryColour = Color(0xFF000000);
+
+
+}
